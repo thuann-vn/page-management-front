e565d5561c39c1dccc77b0694f9b243b04981f03
Thua Nguyen <thua@vsee.com>
2020-08-09 09:19:12 +0700

    Add api url

--- src/constants/Config.js
+++ src/constants/Config.js
@@ -1,5 +1,5 @@
 export default {
-    apiUrl: 'http://localhost:8080',
+    apiUrl: process.env.API_URL || 'http://localhost:8080',
     facebookID: '302064867211847',
     facebookSecret: '58643928fca5597ae9e9e9735a323657',
     pusherAppKey: '3284fd45b16e22872327',
