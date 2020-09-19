de05dadd093b9f7d1a7e7b7e592473138527c5a8
Thua Nguyen <thua@vsee.com>
2020-09-19 21:09:06 +0700

    Update ENV Config

--- src/constants/Config.js
+++ src/constants/Config.js
@@ -1,7 +1,7 @@
 require('dotenv').config();
 
 export default {
-    apiUrl: process.env.API_URL || 'http://localhost:8080',
+    apiUrl: process.env.REACT_APP_API_URL || 'http://localhost:8080',
     facebookID: '302064867211847',
     facebookSecret: '58643928fca5597ae9e9e9735a323657',
     pusherAppKey: '3284fd45b16e22872327',
